f138c3c7dd482fdaadddc31652841b97ddd3b54a
Techi-Joe <techi.jo3@gmail.com>
2024-06-18 00:03:16 -0500

    more structuring

---
+++ src/app_usage_gui/gui/main_window.py
@@ -0,0 +1,5 @@
+class MainWindow():
+    def __init__(self):
+        print("init")
+    def start(self):
+        print("start")
---
+++ src/app_usage_gui/main.py
@@ -0,0 +1,8 @@
+from gui.main_window import *
+
+def main():
+    win = MainWindow()
+    win.start()
+
+if __name__ == "__main__":
+    main()
