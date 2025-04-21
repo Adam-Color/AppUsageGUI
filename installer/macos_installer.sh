58b2f762bb0b84151f54e86749cd36bf563ef235
Techi-Joe <techi.jo3@gmail.com>
2025-04-21 16:39:42 -0400

    finish macos install script

--- installer/macos_installer.sh
+++ installer/macos_installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script is used to install the macOS version of the application.
 
-echo ls
+create-dmg --volicon src/core/resources/icon.icns --volname AppUsageGUIsetup --window-pos 200 190 --window-size 800 400 --app-drop-link 600 185 --eula LICENSE.txt AppUsageGUI_v2.0.0_MACOS_setup.dmg dist/AppUsageGUI.app
