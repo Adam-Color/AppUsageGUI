3bfd3bfc1412c42ab32ea8095c596d1723488dfc
Techi-Joe <techi.jo3@gmail.com>
2025-05-10 18:23:34 -0400

    small grammar fix

--- installers/macos_installer.sh
+++ installers/macos_installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script is used to create the installer for the macOS version of the application
 
-create-dmg --volicon src/core/resources/icon.icns --volname AppUsageGUIsetup --window-pos 200 190 --window-size 800 400 --app-drop-link 600 185 --eula LICENSE.txt dist/AppUsageGUI_v1.3.0_MACOS_arm64_setup.dmg dist/AppUsageGUI.app
+create-dmg --volicon src/core/resources/icon.icns --volname AppUsageGUIsetup --window-pos 200 190 --window-size 800 400 --app-drop-link 600 185 --eula LICENSE.txt dist/AppUsageGUI_v1.3.0_macOS_arm64_setup.dmg dist/AppUsageGUI.app
