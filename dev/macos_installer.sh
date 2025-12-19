8b2b4849dd112b47847b05756af9fb77194bc92e
Techi-Joe <techi.jo3@gmail.com>
2025-12-18 23:08:55 -0600

    Alter macOS install script for terminal use

--- dev/macos_installer.sh
+++ dev/macos_installer.sh
@@ -2,6 +2,6 @@
 # This script is used to create the installer for the macOS version of the application
 app_version='1.8.2'
 
-mv dist/AppUsageGUI.app dist/AppUsageGUI/
+mv ../dist/AppUsageGUI.app ../dist/AppUsageGUI/
 
-create-dmg --volicon src/core/resources/icon.icns --volname AppUsageGUIsetup --window-pos 200 190 --window-size 800 400 --app-drop-link 600 185 --eula LICENSE.txt dist/AppUsageGUI_v${app_version}_macOS_arm64_setup.dmg dist/
+create-dmg --volicon ../src/core/resources/icon.icns --volname AppUsageGUIsetup --window-pos 200 190 --window-size 800 400 --app-drop-link 600 185 --eula ../LICENSE.txt ../dist/AppUsageGUI_v${app_version}_macOS_arm64_setup.dmg ../dist/
